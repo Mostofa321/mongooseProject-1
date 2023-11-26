070d6199a4330a95deb7190e7e9416eac8a69276
Mostofa321 <hmshumon123@gmail.com>
2023-11-26 14:08:21 +0600

    created api for creating new user (requierment: 1)

--- src/app/modules/user/user.routes.ts
+++ src/app/modules/user/user.routes.ts
@@ -5,12 +5,4 @@ const router = Router();
 
 router.post('/', controllers.createUserController);
 
-router.get('/', controllers.getAllUserController);
-
-router.get('/:userId', controllers.getSingleUserController);
-
-router.put('/:userId', controllers.updateSingleUserController);
-
-router.delete('/:userId', controllers.delateSingleUserController);
-
 export default router;
--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -7,41 +7,6 @@ const createUser = async (userData: IUser) => {
   return result;
 };
 
-// get all users from db
-const getAllUsers = async () => {
-  const result = await User.find({}).select(
-    'username fullName age email address',
-  );
-  return result;
-};
-
-// get single user from db
-const getSingleUser = async (userId: number) => {
-  const result = await User.findOne({ userId: userId }).select('-password');
-  return result;
-};
-
-// update single user to db
-const updateSingleUser = async (userId: number, userData: IUser) => {
-  const result = await User.updateOne({ userId: userId }, { $set: userData });
-  if (result.acknowledged) {
-    const updaterUser = await User.findOne({ userId: userId }).select(
-      '-password',
-    );
-    return updaterUser;
-  }
-};
-
-// delete single user from db
-const deleteSingleUser = async (userId: number) => {
-  const result = await User.deleteOne({ userId: userId });
-  return result;
-};
-
 export default {
   createUser,
-  getAllUsers,
-  getSingleUser,
-  deleteSingleUser,
-  updateSingleUser,
 };
